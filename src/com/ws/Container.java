b2105592ce4a21f6f090e09f5c6a9c1b210f7c55
ksw70913 <pavico0913@gmail.com>
2023-12-21 16:24:27 +0900

    12th, 삭제 시 옵션의 유효성 체크

--- src/com/ws/Container.java
+++ src/com/ws/Container.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Container {
 	private static Scanner sc;
-	
+
 	// 공통적으로 사용되는 자원들을 모아두는 공간 초기화
 	public static void init() {
 		sc = new Scanner(System.in);
 	}
-	
+
 	// 공통적으로 사용되는 자원들을 모아두는 공간 자원 해제
 	public static void close() {
 		sc.close();
