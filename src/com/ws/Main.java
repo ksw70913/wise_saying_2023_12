1b77b9d94e1c57cc3ccb2ab82705146cb325dacb
ksw70913 <pavico0913@gmail.com>
2023-12-26 11:46:38 +0900

    20th, html 추출

--- src/com/ws/Main.java
+++ src/com/ws/Main.java
@@ -1,12 +1,13 @@
-package com.ws; // 폴더 com.ws 안에 있음
+package com.ws;
 
-public class Main { // 메인 클래스
-	public static void main(String[] args) { // 메인 메서드
+public class Main {
+	public static void main(String[] args) {
 
-		Container.init(); // container클래스의 init메서드 실행
+		Container.init();
 
-		new App().run(); // app 메서드(생성자)의 run 메서드 실행
+		new App().run();
 
-		Container.close(); // container클래스의 close메서드 실행
+		Container.close();
 	}
+
 }
--- src/com/ws/wiseSaying/controller/WiseSayingController.java
+++ src/com/ws/wiseSaying/controller/WiseSayingController.java
@@ -1,45 +1,45 @@
-package com.ws.wiseSaying.controller; // 폴더 com.ws.wiseSaying.controller안에 있음
+package com.ws.wiseSaying.controller;
 
-import java.util.List; // 자바 라이브러리에 있는 List클래스 import
+import java.util.List;
 
-import com.ws.Container; // 패키지 com.ws안에 있는 Container import
-import com.ws.Rq; // 패키지 com.ws안에 있는 Rq import
-import com.ws.wiseSaying.entity.WiseSaying; // 패키지 com.ws.wiseSaying.entity 안에 있는 WiseSaying import
-import com.ws.wiseSaying.service.WiseSayingService; // 패키지 com.ws.wiseSaying.service안에 있는 WiseSayingService import
+import com.ws.Container;
+import com.ws.Rq;
+import com.ws.wiseSaying.entity.WiseSaying;
+import com.ws.wiseSaying.service.WiseSayingService;
 
-public class WiseSayingController { // WiseSayingController 클래스
+public class WiseSayingController {
 
-	private WiseSayingService wiseSayingService; // WiseSayingService타입의 private wiseSayingService 변수
+	private WiseSayingService wiseSayingService;
 
-	public WiseSayingController() { // WiseSayingController클래스 생성자
-		wiseSayingService = new WiseSayingService();  // WiseSayingService 객체 리모컨을 wiseSayingService안에 넣음
+	public WiseSayingController() {
+		wiseSayingService = Container.wiseSayingService;
 	}
 
-	public void write() { //void 타입의 write 메서드
-		System.out.print("명언 : "); //명언 : 출력
-		String content = Container.getScanner().nextLine().trim(); // String 타입 변수 content 안에 Container.getScanner()메서드실행
-		System.out.print("작가 : "); //작가: 출력						.nextLine()메서드로 문자열 출력 trim()메서드로 좌우 공백 없앰
-		String author = Container.getScanner().nextLine().trim(); // String 타입 변수 author 안에 Container.getScanner()메서드실행
+	public void write() {
+		System.out.print("명언 : ");
+		String content = Container.getScanner().nextLine().trim();
+		System.out.print("작가 : ");
+		String author = Container.getScanner().nextLine().trim();
 
-		int id = wiseSayingService.write(content, author); //wiseSayingService클래스의 write(매개변수 String타입 content, author) 메서드 실행
-															// 그 값을 int 타입의 id 변수에 넣는다.
-		System.out.printf("%d번 명언이 등록되었습니다.\n", id); // %d번 명언이 등록되었습니다.[다음 열] 출력 %d안에 들어갈 값은 정수타입의 id
+		int id = wiseSayingService.write(content, author);
+
+		System.out.printf("%d번 명언이 등록되었습니다.\n", id);
 	}
 
-	public void list() { //void 타입의 list 메서드 실행
+	public void list() {
 		List<WiseSaying> wiseSayings = wiseSayingService.findAll();
-		// List타입(WiseSaying 타입) wiseSayings변수 안에 wiseSayingService.findAll 메서드를 한 내용의 리모컨을 넣음
-		System.out.println("번호  /  작가  /  명언  "); // 번호  /  작가  /  명언  출력
-		System.out.println("=".repeat(30)); //= 30번 출력 
 
-		for (int i = wiseSayings.size() - 1; i >= 0; i--) { //for 반복문 int 타입 i가 wiseSayings.size() -1 값일때, 끝은 i>=0,반복은 i 1씩감소
-			WiseSaying ws = wiseSayings.get(i); // List타입 wiseSayings 안에 int 타입 i를 넣은 걸 wiseSayings타입의 ws 변수에 넣는다
+		System.out.println("번호  /  작가  /  명언  ");
+		System.out.println("=".repeat(30));
+
+		for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+			WiseSaying ws = wiseSayings.get(i);
 
 			System.out.printf("%d  /  %s  /  %s\n", ws.getId(), ws.getAuthor(), ws.getContent());
-		}	// "%d  /  %s  /  %s[줄바꿈] 출력 ws.getId()은 wiseSayings.getId(i)의 값이 들어가있다. 이하 반복
+		}
 	}
 
-	public void remove(Rq rq) { // void 타입의 remove(매개변수 Rq타입의 rq) 메서드
+	public void remove(Rq rq) {
 
 		int id = rq.getIntParam("id", -1);
 
